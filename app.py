fad213dc5e23f0d265b8fb1fd82b04ca00623f9d
Aastik <aastikyadav1997.ay@gmail.com>
2021-03-26 09:26:24 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+
+class task(db.Model):
+    sno=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(200), nullable=False)
+    desc=db.Column(db.String(200), nullable=False)
+    date_created=db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
+
+@app.route('/', methods=['GET','POST'])
+def hello_world():
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        tsk=task(title=title, desc=desc)
+        db.session.add(tsk)
+        db.session.commit()
+    alltasks=task.query.all()
+    return render_template('index.html',alltasks=alltasks)
+    
+@app.route('/update/<int:sno>', methods=['GET','POST'])
+def update(sno):
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        tsk=task.query.filter_by(sno=sno).first()
+        tsk.title=title
+        tsk.desc=desc
+        db.session.add(tsk)
+        db.session.commit()
+        return redirect("/")
+
+    tsk=task.query.filter_by(sno=sno).first()
+
+    return render_template('update.html',tsk=tsk)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    tsk=task.query.filter_by(sno=sno).first()
+    db.session.delete(tsk)
+    db.session.commit()
+    return redirect('/')
+
+
+if __name__=="__main__":
+    app.run(debug=True, port=8000)
